47103f1de7a86507a6afeaa7fd04576036836403
benaryorg <binary@benary.org>
2020-02-05 20:27:13 +0100

    release/v0.2.2
    Signed-off-by: benaryorg <binary@benary.org>

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ pub use hero::{BaseValue, Hero};
 pub fn app() -> clap::App<'static,'static>
 {
 	clap::App::new("dsa-cli")
-		.version("0.2.1")
+		.version("0.2.2")
 		.author("benaryorg <binary@benary.org>")
 		.about("Calculates DSA Rolls")
 		.setting(clap::AppSettings::SubcommandRequiredElseHelp)
